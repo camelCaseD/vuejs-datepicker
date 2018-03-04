319606b4d1cd2e535eb085869f3484b0406c918d
Brandon Borders <brandon@camelcased.com>
2018-03-03 16:31:15 -0800

    Reconfigure typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 
-export default interface Datepicker extends Vue {
+interface Datepicker extends Vue {
   pageTimestamp: number
   selectedDate: Date
   showDayView: boolean
   showMonthView: boolean
   showYearView: boolean
-  calendarHeight: number = 0
+  calendarHeight: number
 }
+
+export default Datepicker;
